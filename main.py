2c94daca1ee77e26db9c5645d6ff41662e0c2c0d
Lucas <lucasignaciosilva18@gmail.com>
2025-11-12 20:14:34 -0300

    ventana 00

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import pygame
-print("1er dia")
+
+#inicializar
+pygame.init()
+
+#tamano(px), ancho - alto
+width = 800
+height = 600
+
+#display, mostramos la ventana
+ventana = pygame.display.set_mode((width,height))
+
+#!loop de ejecucion
+run = True
+while run == True:
+    #todo- event.get: entrega lista de todos los eventos que toma en el juego(ej. un click) 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:#todo| QUIT: cerrar la ventana o F4
+            run = False  #!Cerramos ventana
+pygame.quit()
